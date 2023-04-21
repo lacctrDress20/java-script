554cfce2c0053c994a113d7e376340c558b3befd
lacctrDress20 <1490244@senati.pe>
2023-04-21 11:18:19 -0500

    java 1

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 alert('Faltan 23 dias para esta gran fecha')
+
+var texto = "¿Cuántos días falta para el dia de la madre?";
+var texto2 = "Qué di mas maravilloso, verdad?";
+ 
+document.write(texto);
